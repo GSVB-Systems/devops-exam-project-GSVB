fa18977f020c23667a8893c012ebc835ea2de82e
GreenzQe <73607229+GreenzQe@users.noreply.github.com>
2026-04-14 15:11:24 +0200

    Updated tests to support feature flags

--- Server/Test/ControllerTests/LeaderboardsControllerTests.cs
+++ Server/Test/ControllerTests/LeaderboardsControllerTests.cs
@@ -1,6 +1,8 @@
 using DevOpsAppApi.Controllers;
+using DevOpsAppApi;
 using DevOpsAppService.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Moq;
 using Test.Builders;
 
@@ -8,13 +10,22 @@ namespace Test.ControllerTests;
 
 public class LeaderboardsControllerTests
 {
+    private static IOptions<FeatureFlagsOptions> EnabledFlags()
+    {
+        return Options.Create(new FeatureFlagsOptions
+        {
+            Admin = true,
+            Leaderboards = true
+        });
+    }
+
     [Fact]
     public async Task Get_ReturnsOk_WithEntries()
     {
         var service = new Mock<ILeaderboardService>();
         var expected = ControllerTestData.LeaderboardEntries(count: 2);
         service.Setup(x => x.GetEntriesAsync(CancellationToken.None)).ReturnsAsync(expected);
-        var controller = new LeaderboardsController(service.Object);
+        var controller = new LeaderboardsController(service.Object, EnabledFlags());
 
         var result = await controller.Get(CancellationToken.None);
 
--- Server/Test/ControllerTests/UserControllerTests.cs
+++ Server/Test/ControllerTests/UserControllerTests.cs
@@ -1,7 +1,9 @@
 using DevOpsAppApi.Controllers;
+using DevOpsAppApi;
 using DevOpsAppContracts.Models;
 using DevOpsAppService.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Moq;
 using Test.Builders;
 
@@ -9,11 +11,20 @@ namespace Test.ControllerTests;
 
 public class UserControllerTests
 {
+    private static IOptions<FeatureFlagsOptions> EnabledFlags()
+    {
+        return Options.Create(new FeatureFlagsOptions
+        {
+            Admin = true,
+            Leaderboards = true
+        });
+    }
+
     [Fact]
     public async Task GetMe_ReturnsUnauthorized_WhenUserClaimMissing()
     {
         var service = new Mock<IUserService>();
-        var controller = new UserController(service.Object);
+        var controller = new UserController(service.Object, EnabledFlags());
         ControllerTestHelpers.SetUser(controller);
 
         var result = await controller.GetMe();
@@ -27,7 +38,7 @@ public class UserControllerTests
         var service = new Mock<IUserService>();
         service.Setup(x => x.GetByIdAsync("u-1")).ReturnsAsync((UserDto?)null);
 
-        var controller = new UserController(service.Object);
+        var controller = new UserController(service.Object, EnabledFlags());
         ControllerTestHelpers.SetUser(controller, "u-1");
 
         var result = await controller.GetMe();
@@ -43,7 +54,7 @@ public class UserControllerTests
         expected.UserId = "u-1";
         service.Setup(x => x.GetByIdAsync("u-1")).ReturnsAsync(expected);
 
-        var controller = new UserController(service.Object);
+        var controller = new UserController(service.Object, EnabledFlags());
         ControllerTestHelpers.SetUser(controller, "u-1");
 
         var result = await controller.GetMe();
@@ -57,7 +68,7 @@ public class UserControllerTests
     {
         var service = new Mock<IUserService>();
         service.Setup(x => x.DeleteAsync("u-1")).ReturnsAsync(true);
-        var controller = new UserController(service.Object);
+        var controller = new UserController(service.Object, EnabledFlags());
 
         var result = await controller.Delete("u-1");
 
@@ -69,7 +80,7 @@ public class UserControllerTests
     {
         var service = new Mock<IUserService>();
         service.Setup(x => x.DeleteAsync("u-1")).ReturnsAsync(false);
-        var controller = new UserController(service.Object);
+        var controller = new UserController(service.Object, EnabledFlags());
 
         var result = await controller.Delete("u-1");
 
@@ -82,7 +93,7 @@ public class UserControllerTests
         var service = new Mock<IUserService>();
         var request = ControllerTestData.LoginRequest();
         service.Setup(x => x.LoginAsync(request)).ReturnsAsync((AuthResponseDto?)null);
-        var controller = new UserController(service.Object);
+        var controller = new UserController(service.Object, EnabledFlags());
 
         var result = await controller.Login(request);
 
@@ -96,7 +107,7 @@ public class UserControllerTests
         var expected = ControllerTestData.AuthResponse();
         var request = ControllerTestData.LoginRequest(seed: 107);
         service.Setup(x => x.LoginAsync(request)).ReturnsAsync(expected);
-        var controller = new UserController(service.Object);
+        var controller = new UserController(service.Object, EnabledFlags());
 
         var result = await controller.Login(request);
 
